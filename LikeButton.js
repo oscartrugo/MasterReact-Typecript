8c24f8fa35ca449d024cd8a511db1e0559e1afb4
Oscar Trujillo <oscar.trujillo@atos.net>
2022-02-23 13:43:49 -0600

    6. Webpack overview

---
+++ LikeButton.js
@@ -0,0 +1,19 @@
+class LikeButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { liked: false };
+    }
+
+    render() {
+        if (this.state.liked) {
+            return 'You liked this.';
+        }
+
+        return React.createElement('div', {}, React.createElement(
+            'button',
+            { onClick: () => this.setState({ liked: true})},
+            'Like'
+
+        ));
+    }
+}
