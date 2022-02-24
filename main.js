0350ba084bb7c78dbc1b809f0772e4d44d416dd4
Oscar Trujillo <oscar.trujillo@atos.net>
2022-02-24 01:40:43 -0600

    22. Class

--- main.js
+++ main.js
@@ -1,33 +1,60 @@
-const fruits = [undefined, 'guava', 'coconout'];
+class Shape {
+    #privateVariable = 'privateVariable'
+    publicVariable = 'publicVariable'
+    static staticVariable = 'staticVariable'
 
-const [firstFruit = 'strawberry', ...otherFruits] = fruits
+    constructor(height, width){ //height and width are private variables
+        this.height = height; //declaring public variable
+        this.width = width; //declaring public variable
+    }
 
-console.log(firstFruit)
-console.log(otherFruits)
-//---------------------------------------------------------------------------
-// const fruits = ['apple', 'avocado']
+    getArea(){
+        return 'not implemented'
+    }
 
-// const newFruits = [...fruits, 'guava']
+    getPrivateData(){
+        return this.#privateVariable; //Value fetched of a private variable
+    }
+}
 
-// console.log('fruits', fruits)
-// console.log('newFruits', newFruits)
-//---------------------------------------------------------------------------
-// const userDetails = {
-//     name: 'Oscar',
-//     address: 'Mexico',
-//     sample: {
-//         test: 'test'
-//     }
-// }
+class Square extends Shape{
+    getArea(){
+        return this.height * this.width
+    }
 
-// const {name: newName = 'Trujillo', ...otherDetails} = userDetails
+    getParentClassArea(){
+        return super.getArea(); //Calling a parent attribute in the child class
+    }
+}
 
-// console.log(newName)
-// console.log(otherDetails)
+const mySquare = new Square(5, 8);
 
-// const newUserDetails = { ...userDetails, mobileNumber: 123}
+console.log(mySquare.getPrivateData())
+//console.log(mySquare.getParentClassArea())
 
-// newUserDetails.sample.test = 'newTest';
+//const myShape = new Shape(5, 10);
 
-// console.log('userDetails: ', userDetails)
-// console.log('newUserDetails: ', newUserDetails)
+// console.log(myShape.height)
+// console.log(myShape.width)
+// //we can access a public variable
+// console.log(myShape.publicVariable)
+//Calling an static variable
+//console.log(Shape.staticVariable)
+
+/**
+ * When to use a static variable? And when to use a public variable?
+ * You will only use public variable if you want that variable to only leave when you instantiate
+ * the class.
+ * 
+ * But if you want to create a variable that will be globally accessible, even if you change the value,
+ * then you can actually use a static variable, because you can access it without instantiating the class.
+ * 
+ * 
+ * How can we access private variable?
+ * You need to create a function to return the value of it. 
+ * 
+ * Whats the use of a private variable?
+ * If you have a data that you actually use, for example, computing the area, and you don't want to expose 
+ * that data on how you compute the area, then you can actually create a private variable, so that the user
+ * of the shape class or the square class doesn't know how you calculate the value of the getArea
+ */;
