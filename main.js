61846d863f37e076c47d2f781ebdedcc49edcf80
Oscar Trujillo <oscar.trujillo@atos.net>
2022-02-24 17:40:42 -0600

    26. Promises and Async + Await

--- main.js
+++ main.js
@@ -1,45 +1,58 @@
-class Printer{
-    name= "I am Printer Class" //public variable
+let myNumber = 5;
 
-    printName(){
-        console.log('printName: ', this.name);
-    }
-
-    printNameArrowFn = () => {
-        console.log('printNameArrowFn: ', this.name);
-    }
+function changeNumber(){
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            myNumber = 10;
+            resolve(myNumber);
+        }, 500);
+    })
 }
 
-const myPrinter = new Printer();
-// myPrinter.printName()
-// myPrinter.printNameArrowFn()
+async function startChange(){ //Add async to make starChange function asynchronous
+    await changeNumber(); //It will wait for a changeNumber function before it runs the console.log
+    console.log(myNumber);
 
-const customPrinter = {
-    name: "I am Custom Printer",
-    printName: myPrinter.printName,
-    printNameArrowFn: myPrinter.printNameArrowFn,
 }
 
-//customPrinter.printName();
-customPrinter.printNameArrowFn();
+startChange();
+
+// function calculateSquareArea() {
+//     return new Promise(function(resolve, reject){
+//         setTimeout(() => {
+//             const squareArea = 5*5;
+//             //resolve(squareArea)
+//             resolve(squareArea)
+//         }, 500)
+//     })
+// }
+
+// const myPromises = [ //Array of promises
+//     new Promise((resolve, reject) => setTimeout(() => reject(100), 100)),
+//     new Promise((resolve, reject) => setTimeout(() => reject(200), 200)),
+//     new Promise((resolve, reject) => setTimeout(() => resolve(300), 300)) // reject = catch: undefined. 
+//     ];
+
+// function onFulFilled(data){
+//     console.log('onFulFilled: ', data)
+// }
+
+// function onRejected(reason){
+//     console.log('onRejected: ', reason)
+// }
+
+//For Promise.all, returns rejected with at least one reject instead of resolve
+//For Promise.allsettled it will just wait for all promises to be settlet, that's it
+//For Promise.any it will actually wait for one promise in the array of promises to be fulfilled,
+// which ever will complete first
+//For Promise.race it will actually look for either reject or resolve, whatever runs first. 
+
+// Promise.any(myPromises)
+// .then(onFulFilled)
+// .catch((reason) => {
+//     console.log('catch: ', reason)
+// })
 
 /**
- * When to use a static variable? And when to use a public variable?
- * You will only use public variable if you want that variable to only leave when you instantiate
- * the class.
- * 
- * But if you want to create a variable that will be globally accessible, even if you change the value,
- * then you can actually use a static variable, because you can access it without instantiating the class.
- * 
- * 
- * How can we access private variable?
- * You need to create a function to return the value of it. 
- * 
- * Whats the use of a private variable?
- * If you have a data that you actually use, for example, computing the area, and you don't want to expose 
- * that data on how you compute the area, then you can actually create a private variable, so that the user
- * of the shape class or the square class doesn't know how you calculate the value of the getArea
- * 
- * If we are using Arrow function, it doesn't care whether you actually execute the funciton. What matters
- * most is where you created the arrow function, the scope.
- */;
+ * We can actually catch the reject of our promise and the error of our .then function 
+ */
