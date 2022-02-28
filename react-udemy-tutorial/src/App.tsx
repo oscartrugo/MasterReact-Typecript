9ed3c78f5068bdae223770afdb25f846d4798d19
oscartrugo <oscartrugo@gmail.com>
2022-02-28 02:03:12 -0600

    39. Lifecycle - Updating

--- react-udemy-tutorial/src/App.tsx
+++ react-udemy-tutorial/src/App.tsx
@@ -3,28 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 import CounterManagement from './components/CounterManagement';
 
-interface AppState {
-  change: boolean
-}
-
-class App extends React.Component<{}, AppState> {
-  constructor(props: {}) {
-    super(props);
-    this.state = {
-      change: true
-    }
-  }
-
-  clickButton = () => {
-    this.setState({change: !this.state.change})
-  }
-
+class App extends React.Component {
   render(){
     return(
       <>
         <h1>My App</h1>
-        {this.state.change && <CounterManagement ownerName='Oscar'/>}
-        <button onClick={this.clickButton}>Change</button>
+        <CounterManagement ownerName='Oscar'/>
       </>
     )
   }
--- react-udemy-tutorial/src/components/CounterManagement/index.tsx
+++ react-udemy-tutorial/src/components/CounterManagement/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { CounterManagementProps, CounterManagementState } from "./interface";
+import { CounterManagementProps, CounterManagementState, UserDataAPI } from "./interface";
 import axios from "axios";
 //class component
 class CounterManagement extends React.Component<CounterManagementProps, CounterManagementState> {
@@ -7,96 +7,89 @@ class CounterManagement extends React.Component<CounterManagementProps, CounterM
         super(props);
 
         this.state = {
-            counter: 0, //Propertie
-            users: [] //Propertie
+            user: 1, //Propertie
+            userData: {
+                id: 1,
+                email: '',
+                first_name: '',
+                last_name: '',
+                avatar: ''
+            }
         }
 
         console.log("constructor")
     }
 //Creamos las funciones que controlaran el resultado de dar click en los botones
     handleAddClick = () =>{
-        this.setState({ counter: this.state.counter + 1})
+        this.setState({ user: this.state.user + 1}) //, () => this.fetchUserData
     }
 
     handleMinusClick = () =>{
-        this.setState({ counter: this.state.counter - 1})
+        this.setState({ user: this.state.user - 1})
+
     }
 
-    static getDerivedStateFromProps(props: CounterManagementProps, state: CounterManagementState) {
-        console.log('getDerivedStateFromProps');
-        //return props.ownerName === 'Oscar' ? { counter: 5 } : null;
-        return null;
+    // static getDerivedStateFromProps(props: CounterManagementProps, state: CounterManagementState) {
+    //     console.log('getDerivedStateFromProps');
+
+    //     return null;
+    // }
+
+    // shouldComponentUpdate(nextProps: CounterManagementProps, nextState: CounterManagementState) {
+    //     console.log('shouldComponentUpdate');
+
+    //     return true
+    //     //return this.state.counter < 5; //Después de 5, la UI no se actualiza.
+    // }
+    // //this will return the data you need before React updates the DOM itself
+    // getSnapshotBeforeUpdate(prevProps: CounterManagementProps, prevState: CounterManagementState) {
+    //     console.log('getSnapshotBeforeUpdate');
+
+    //     return { scrollPosition: '152px' };
+    // }
+
+    fetchUserData = () => {
+        axios.get(`https://reqres.in/api/users/${this.state.user}`)
+            .then(response => {
+                const userDataAPI = response.data as UserDataAPI;
+
+                this.setState({
+                    userData: userDataAPI.data
+                })
+            });
     }
 
-    clickWindow = () => {
-        console.log("clickWindow Event occur")
-        this.setState({ counter: this.state.counter + 1 }); //The value of the counter increases 1
+    componentDidMount(){
+        this.fetchUserData();
     }
 
-    componentDidMount() { //This functions is used to fetch data from the API
-        axios.get("https://reqres.in/api/users?page=2")
-        .then(response => {
-            const data = response.data;
-
-            const users = data.data.map((userData: any) => userData.first_name); //Fetch the name in the API
-
-            this.setState({users})
-        })
-        window.addEventListener('click', this.clickWindow);
+    componentDidUpdate(prevProps: CounterManagementProps, prevState: CounterManagementState, snapshot: any) {
+        if(prevState.user !== this.state.user){ //Si el estado anterior del user no es igual al estado de este usuario, hace fetch
+            this.fetchUserData();
+        }
     }
 
-    componentWillUnmount() {
-        window.removeEventListener('click', this.clickWindow);
-    }
+    // componentDidMount() {
+    //     this.fetchUserData();
+    // }
 
 
     render() {
         console.log("render")
         const { ownerName } = this.props;
-        const {counter, users} = this.state;
+        const { user, userData } = this.state;
+        const {first_name} = userData;
         return (
             <div>
                 <h1>Counter Management</h1>
                 <h2>Owner name: {ownerName}</h2>
-
-                <h3>Counter: {counter} </h3>
+                <h3>UserID: {user} </h3>
+                <h3>{first_name}</h3>
                 <button onClick={this.handleAddClick}>Add</button>
                 <button onClick={this.handleMinusClick}>Minus</button>
-                <ul>
-                    {users.map(user => <li>{user}</li>)}
-                </ul>
             </div>
         )
     }
 }
 
 export default CounterManagement;
-
-/**
- * Different ways to set a state
- * 
- * 1. Function (if you want to update the value state from the previous value)
- * 2. Object
- * 
- * Remember HOW IMPORTANT is to use an ARROW FUNCTION in HandleAddClick
- * If you use a standard function the "this" of the function will refer to the button,
- * 
- * But if we use an Arrow Function, thar "this" will actually refer to our counter management component
- *
- * 
- */
-
-
-/**
- * If you need to fetch data for your UI, you will actually call the API in the componentDidMount, because
- * componentDidMount will only be called once
- */
-
-/**
- * You wil use getDerivedStateFromProps if you want to override the value of state depending on props
- * But you should only use this on rare use cases, because this will make it complex to maintain your
- * component.
- * 
- * And componentDidMount will be called only once when our component mounts.
- * This is the function where we will call our API to get some data and to render to our component.
- */
