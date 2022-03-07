d87d50c46ecb484cb4eca302787ed5b5954d6a96
oscartrugo <oscartrugo@gmail.com>
2022-03-06 20:25:31 -0600

    67. Unmount Component

--- react-udemy-tutorial/src/index.tsx
+++ react-udemy-tutorial/src/index.tsx
@@ -19,6 +19,9 @@ ReactDOM.render(
   () => { console.log('it renders!') }
 );
 
+const hasUnmountRenderSampleComponent = ReactDOM.unmountComponentAtNode(document.getElementById('renderSample')!);
+console.log('hasUnmountRenderSampleComponent:', hasUnmountRenderSampleComponent);
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
