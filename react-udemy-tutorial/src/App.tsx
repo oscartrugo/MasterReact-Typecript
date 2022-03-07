8203039fa22ba90c687321146df49460b03c0849
oscartrugo <oscartrugo@gmail.com>
2022-03-07 13:02:09 -0600

    80. Link and redirect

--- react-udemy-tutorial/src/App.tsx
+++ react-udemy-tutorial/src/App.tsx
@@ -2,16 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './components/HomePage';
-import { Router, Route, Switch } from 'react-router';
+import { Router, Route, Switch, Redirect } from 'react-router';
 import AboutPage from './components/AboutPage';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Link } from 'react-router-dom';
+import ProfilePage from './components/ProfilePage';
+import { SpecialLink } from './components/SpecialLink';
 
 function App() {
   return (
     <BrowserRouter>
+      <Link component={SpecialLink} to="/home">Home</Link>
+      <br/>
+      <Link component={SpecialLink} to="/about">About</Link>
+      <br/>
+      <Link component={SpecialLink} to="/profile">Profile</Link>
+      <Switch>
         <Route component={AboutPage} path="/about" />
-        <Route render={(routeProps) =>  <HomePage {...routeProps} />} path="/home" />
-        <Route children={(routeProps) => routeProps.match ? <HomePage {...routeProps} /> : <div>Does not match</div>} path="/children" />
+        <Route component={HomePage} path="/home" />
+        <Route component={ProfilePage} path="/profile" />
+        <Redirect to="/home" />
+      </Switch>
     </BrowserRouter>
   );
 }
---
+++ react-udemy-tutorial/src/components/ProfilePage/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ProfilePage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1> Profile Page </h1>
+            </div>
+        )
+    }
+}
+
+export default ProfilePage;
