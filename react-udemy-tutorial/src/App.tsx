49e5790f0b61e7d69c4febfe661e0709582b08a6
oscartrugo <oscartrugo@gmail.com>
2022-03-06 19:41:28 -0600

    62. Summary

--- react-udemy-tutorial/src/App.tsx
+++ react-udemy-tutorial/src/App.tsx
@@ -14,3 +14,70 @@ function App() {
 }
 
 export default App;
+
+/**
+ * 
+ *    SUMMARY
+ * Component - Te permite dividir la UI en independientes, reutilizables piezas, y pensar acerca
+ * de cada piesa en aislamiento.
+ * 
+ * Props - input/parameters - Puedes utilizarlos para dinámicamente cargar la UI.
+ * 
+ * State - Data dentro de tu componente que va a activa una recarga cuando la actualizas.
+ * 
+ * Lifecycle 
+ *    - Mounting
+ *        - getDeriveredStateFromProps
+ *        - render
+ *        - componentDidMount
+ *
+ *    - Unmounting
+ *        - componentWillUnmount
+ * 
+ *    - Updating
+ *        - getDerivedStateFromProps
+ *        - shouldComponentUpdate
+ *        - render
+ *        - getSnapshotBeforeUpdate
+ *        - componentDidUpdate
+ *      
+ *    - Lifecycle
+ *        - Error Handling
+ *            - getDerivedStateFromError
+ *            - componentDidCatch
+ * 
+ *    - Lifecycle - deprecated & unsafe
+ *         - componentWillMount
+ *         - componentWillReceiveProps
+ *         - componentWillUpdate
+ * 
+ *    - Functional Component - without lifecycle
+ *    
+ *    - Class Component - with state and lifecycle
+ * 
+ *    - React.PureComponent - impplements shouldComponentUpdate with shallow checks for props and state
+ *    
+ *    - React.Memo - shallow check for props in functional component
+ *      
+ *    - Higher Order Component - a pattern to reuse component logic using function that will return a component
+ * 
+ *    - RenderProps - patter to reuse component logic by passing component as a prop
+ * 
+ *    - List - mostly use in array of components/elements so React know which to add/update quickly
+ * 
+ *    - Refs - use to reference an element
+ *        - createRef
+ *        - callbackRef
+ *        - Forwarding Ref
+ * 
+ *    - Context - to share data/props in multiple components
+ *  
+ *    - Lazy & Suspense - use for code splitting with fallback feature while waiting for the code
+ * 
+ *    - React.Children - to easily manage this.props.children
+ * 
+ *    - React.cloneElement - to clone an element/componet
+ * 
+ *    - React.isValidElement - to verify that an obect is an element
+ * 
+ */
